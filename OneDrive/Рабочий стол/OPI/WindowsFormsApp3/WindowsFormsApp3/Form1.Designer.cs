9c82a3c087cc950ff11483cb205debe0f24e5ed2
IT-BLOK\User <maistruk2001@gmail.com>
2020-12-21 17:07:51 +0200

    Message

---
+++ OneDrive/Рабочий стол/OPI/WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kboard = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Monitor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sets = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Label = new System.Windows.Forms.Label();
+            this.numbertxt = new System.Windows.Forms.TextBox();
+            this.ADD = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Kboard,
+            this.Monitor,
+            this.Sets,
+            this.Delete});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(546, 300);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "PC";
+            this.ID.Name = "ID";
+            // 
+            // Kboard
+            // 
+            this.Kboard.HeaderText = "Keyboard and mous";
+            this.Kboard.Name = "Kboard";
+            // 
+            // Monitor
+            // 
+            this.Monitor.HeaderText = "Monitor";
+            this.Monitor.Name = "Monitor";
+            // 
+            // Sets
+            // 
+            this.Sets.HeaderText = "Sets";
+            this.Sets.Name = "Sets";
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "Delete";
+            this.Delete.Name = "Delete";
+            this.Delete.Text = "Delete";
+            this.Delete.UseColumnTextForButtonValue = true;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Label.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label.Location = new System.Drawing.Point(12, 324);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(103, 21);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "Number PC:";
+            // 
+            // numbertxt
+            // 
+            this.numbertxt.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.numbertxt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numbertxt.Location = new System.Drawing.Point(121, 323);
+            this.numbertxt.Name = "numbertxt";
+            this.numbertxt.Size = new System.Drawing.Size(100, 22);
+            this.numbertxt.TabIndex = 2;
+            // 
+            // ADD
+            // 
+            this.ADD.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ADD.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ADD.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ADD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ADD.Location = new System.Drawing.Point(16, 360);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(75, 32);
+            this.ADD.TabIndex = 3;
+            this.ADD.Text = "Add row";
+            this.ADD.UseVisualStyleBackColor = false;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 404);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.numbertxt);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kboard;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Monitor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sets;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TextBox numbertxt;
+        private System.Windows.Forms.Button ADD;
+    }
+}
+
---
+++ OneDrive/Рабочий стол/OPI/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Add(1, "ON", "ON", "ON");
+            dataGridView.Rows.Add(2, "ON", "ON", "ON");
+            dataGridView.Rows.Add(3, "ON", "ON", "ON");
+            dataGridView.Rows.Add(4, "ON", "ON", "ON");
+            dataGridView.Rows.Add(5, "ON", "ON", "ON");
+
+            
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView.Columns[e.ColumnIndex].Name == "Delete")
+            {
+                int index = e.RowIndex;
+                if(MessageBox.Show("Are you sure want to delete this row ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) 
+                    == DialogResult.Yes)
+                {
+                    dataGridView.Rows.RemoveAt(index);
+                }
+            }
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Add(numbertxt.Text, "ON", "ON", "ON");
+
+        }
+    }
+}
